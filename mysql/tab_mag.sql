96ff78f1f542e76f025494855908db20ddfebbc5
66goddness <ziyuzhe@qq.com>
2021-05-26 16:42:57 +0800

    two commit

---
+++ mysql/tab_mag.sql
@@ -0,0 +1,8 @@
+INSERT INTO `tab_mag` VALUES ('甲', '111', '');
+INSERT INTO `tab_mag` VALUES ('乙', '222', '');
+INSERT INTO `tab_mag` VALUES ('丙', '333', '');
+INSERT INTO `tab_mag` VALUES ('丁', '2323', '南昌');
+INSERT INTO `tab_mag` VALUES ('丁', '2323', '南昌');
+INSERT INTO `tab_mag` VALUES ('欢欢', '2424', '北京');
+INSERT INTO `tab_mag` VALUES ('贝贝', '2222', '上海');
+INSERT INTO `tab_mag` VALUES ('undefined', 'undefined', 'undefined');
